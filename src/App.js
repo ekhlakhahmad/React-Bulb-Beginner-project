1b1853b43f93413492d136b89b2a2a8022a54ea1
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-01-26 17:45:34 +0530

    almost completed✅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Bulb from "./Bulb";
+
+const App = () => {
+  return (
+    <div>
+      <Bulb />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Bulb.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import onImage from "./images/ON.png";
+import offImage from "./images/OFF.png";
+
+const Bulb = () => {
+  const [on, setOn] = useState(true);
+  return (
+    <div className="bulb">
+      {on ? (
+        <img src={onImage} alt="bulb on" />
+      ) : (
+        <img className="image-off" src={offImage} alt="bulb on" />
+      )}
+
+      <div className="ONOFF">
+        <button onClick={() => setOn(true)}>ON</button>
+        <button onClick={() => setOn(false)}>OFF</button>
+      </div>
+    </div>
+  );
+};
+
+export default Bulb;
